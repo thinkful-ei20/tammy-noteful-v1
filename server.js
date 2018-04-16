69adaa12fad4146dbc62eb1fbada85a05c582064
Tammy Tian <tiantammy@outlook.com>
2018-04-17 05:21:24 +0800

    adjusted package.json to include nodemon, created id parameter and query endpoints

--- server.js
+++ server.js
@@ -1,7 +1,41 @@
 'use strict';
-
 const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
-// INSERT EXPRESS APP CODE HERE...
+//EXPRESS APP CODE HERE...
+
+//import express
+const express = require('express');
+
+//init epress app
+const app = express();
+
+//retrieve static assets in public
+app.use(express.static('public'));
+
+// app.get('/api/notes/',(req, res) => {
+//   res.json(data);
+// });
+  
+
+app.get('/api/notes/?',(req, res) => {
+  let searchTerm = req.query.searchTerm;
+  console.log(searchTerm);
+  let filteredData = data.filter(item => item.title.includes(searchTerm));
+  res.json(filteredData);
+});
+
+app.get('/api/notes/:id',(req, res) => {
+  let id = req.params.id;
+  console.log(id);
+  let returnNote = data.find(item => item.id === Number(id));
+  res.json(returnNote);
+});
+
+
+app.listen(8080, function(){
+  console.info(`Server is listining on ${this.address().port}`);
+}).on('error', err => {
+  console.error(err);
+});
