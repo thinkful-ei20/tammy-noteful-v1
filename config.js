aa33e392b897fe034eeae77717670924bb02d578
Tammy Tian <tiantammy@outlook.com>
2018-04-18 04:43:03 +0800

    added a return test in idsearch, testing errors

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 'use strict';
+const PORT = 8080;
 
-module.exports.PORT = 8080;
+module.exports = {PORT};
--- server.js
+++ server.js
@@ -8,6 +8,7 @@ console.log('Hello Noteful!');
 //import express
 const express = require('express');
 const {PORT} = require('./config');
+const requestLogger = require('./middlewares/logger');
 
 //init epress app
 const app = express();
@@ -15,25 +16,48 @@ const app = express();
 //retrieve static assets in public
 app.use(express.static('public'));
 
+
+app.use(requestLogger);
+
+
 // app.get('/api/notes/',(req, res) => {
 //   res.json(data);
 // });
-  
 
-app.get('/api/notes/?',(req, res) => {
+app.get('/api/notes',(req, res) => {
   let searchTerm = req.query.searchTerm;
-  console.log(searchTerm);
+  console.log("The search term is", searchTerm);
   let filteredData = data.filter(item => item.title.includes(searchTerm));
   res.json(filteredData);
 });
 
 app.get('/api/notes/:id',(req, res) => {
   let id = req.params.id;
-  console.log(id);
+  console.log("The id is", id);
   let returnNote = data.find(item => item.id === Number(id));
+  if (!returnNote) {
+    return res.status(404).json({message: 'Not Found'});
+  }
   res.json(returnNote);
 });
 
+app.use(function (req, res, next) {
+  let err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
 
 app.listen(PORT, function(){
   console.info(`Server is listining on ${this.address().port}`);
