c3b8be939734b3211c98e22acc607cfdfd636693
Tammy Tian <tiantammy@outlook.com>
2018-04-19 03:34:31 +0800

    added delete and updated client

--- router/notes.router.js
+++ router/notes.router.js
@@ -21,7 +21,7 @@ router.get('/notes/:id',(req, res,next) => {
   // id = Number(id);
   // console.log('The id is', id);
   notes.find(id, (err, returnNote) => {
-    if (!err) {
+    if (err) {
       return next(err);
     } 
     if (returnNote) {
@@ -60,5 +60,37 @@ router.put('/notes/:id', (req, res, next) => {
   });
 });
 
+router.post('/notes', (req, res, next) => {
+  const { title, content } = req.body;
+
+  const newItem = { title, content };
+  /***** Never trust users - validate input *****/
+  if (!newItem.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  notes.create(newItem, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+router.delete('/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  notes.delete(id, err => {
+    if (err) {
+      return next(err);
+    }  
+    res.sendStatus(204);
+  });
+});
 
 module.exports = router;
