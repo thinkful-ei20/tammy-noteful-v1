aa92d6844266442dc01ba7d6245107522d92973e
Tammy Tian <tiantammy@outlook.com>
2018-04-21 04:39:00 +0800

    added tests for get

--- test/server.test.js
+++ test/server.test.js
@@ -40,3 +40,54 @@ describe('404 handler', function () {
       });
   });
 });
+
+describe('GET /api/notes', function() {
+  it('should return the default of 10 notes as an array', function (){
+    return chai.request(app)
+      .get('/api/notes')
+      .then(function (res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body.length).to.be.at.least(10);
+      });
+  });
+
+  it('should return an array of objects with keys id, title, and content', function (){
+    return chai.request(app)
+      .get('/api/notes')
+      .then(function(res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        res.body.forEach(function(item) {
+          expect(item).to.be.a('object');
+          expect(item).to.have.all.keys(
+            'id', 'title', 'content');
+        });
+      });
+  });   
+
+  it('should return the correct search results for a valid query', function (){
+    return chai.request(app)
+      .get('/api/notes?searchTerm=life')
+      .then(function (res) {
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body).to.have.length(1);
+        expect(res.body[0]).to.be.an('object');
+      });
+  });
+
+  it('should return an empty array for an incorrect query', function() {
+    return chai.request(app)
+      .get('/api/notes?searchTerm=68675')
+      .then(function (res){
+        expect(res).to.have.status(200);
+        expect(res).to.be.json;
+        expect(res.body).to.be.a('array');
+        expect(res.body).to.have.length(0);
+      });
+  });
+});
