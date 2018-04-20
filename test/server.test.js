53c2aec6b5ce13bfb1bea2a5eb08158d2b5e31d1
Tammy Tian <tiantammy@outlook.com>
2018-04-21 03:16:49 +0800

    added test for GET and 404 error

--- test/server.test.js
+++ test/server.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const app = ('../server');
+const app = require('../server');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 
@@ -16,5 +16,27 @@ describe('New check', function(){
 
   it('2+2 should equal 4', function (){
     expect(2 + 2).to.equal(4);
+  });
+});
+
+describe('Express static', function() {
+  it('should return index page when GET request "/" called', function () {
+    return chai.request(app)
+      .get('/')
+      .then(function (res) {
+        expect(res).to.exist;
+        expect(res).to.have.status(200);
+        expect(res).to.be.html;
+      });
+  });
+});
+
+describe('404 handler', function () {
+  it('should respond with 404 when given a bad path', function() {
+    return chai.request(app)
+      .get('/DOES/NOT/EXIST')
+      .then(res => {
+        expect(res).to.have.status(404);
+      });
   });
 });
